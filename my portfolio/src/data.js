67ba0b73bf5eaef8ac1f20204947f321a0ab36c6
Tanya Milburn <tanyamilburn89@gmail.com>
2022-09-27 20:28:52 -0600

    setting up files

---
+++ my portfolio/src/data.js
@@ -0,0 +1,28 @@
+export const projects = [
+    {
+      title: "Music Weather App",
+      subtitle: "Javascript",
+      description:
+        "This is my first vanilla javascript project working with a music API. You can submit your current location, and based on the current weather, the app will select a song to match the mood.",
+      image: "./musicweatherapp.png",
+      link: "https://tanyamilburn.github.io/birthday-cake/index.html",
+    },
+    {
+      title: "The Wedding Loop",
+      subtitle: "MySQL, Express.js, MVC",
+      description:
+        "This app lets you plan a wedding and its associated events. A unique code is given to your guests, so they can sign up and be kept up to date with all the wedding info!",
+      image: "./weddingloop.png",
+      link: "https://limitless-journey-28555.herokuapp.com/",
+    }
+]
+
+export const skills = [
+    "Express.js",
+    "React",
+    "MongoDb",
+    "Node",
+    "GraphQL",
+    "MySQL",
+  ];
+  
